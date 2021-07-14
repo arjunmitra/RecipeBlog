75fed12cf4958c3c1ae088aa4077fb328eb85018
Arjun Mitra <mitraarj@usc.edu>
2021-07-14 17:29:47 +0530

    created Post, author and category models

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index, blog, post
+from django.conf import settings
+from django.conf.urls.static import static
+from post.views import index, blog, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog),
     path('post/', post),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
