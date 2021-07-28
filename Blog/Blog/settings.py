fd606239c8590e45f318b3ef5dbe36a3e7cfb50e
Arjun Mitra <mitraarj@usc.edu>
2021-07-28 17:53:14 +0530

    added Procfile and requirements.txt

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h8t23t(*ktn7q=x^x(k0l4zxy+pc)@89hu!z)h(6+rjgueb$h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0:5000','0.0.0.0']
 
 
 # Application definition
